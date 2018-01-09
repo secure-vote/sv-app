5479a9e752e8dd7f9bf984c314046b00cbac3d58
Thomas Sesselmann <sessel52@gmail.com>
2018-01-09 15:05:39 +1100

    Add loading screen + spinner

--- web/index.js
+++ web/index.js
@@ -4,6 +4,10 @@ setTimeout(() => {
     const Elm = require('./src/Main.elm');
     const app = Elm.Main.embed(document.getElementById('sv-fullscreen'));
 
+    // Remove Loading Spinner
+    var element = document.getElementById("loading-screen");
+    element.parentNode.removeChild(element);
+
 }, 400);
 
 
