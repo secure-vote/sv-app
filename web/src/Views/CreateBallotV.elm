6e9bbebc7545016053e07767fbd8f38473f0fe73
Thomas Sesselmann <sessel52@gmail.com>
2018-01-30 15:15:09 +1100

    Refactor Create New Ballot View

--- web/src/Views/CreateBallotV.elm
+++ web/src/Views/CreateBallotV.elm
@@ -3,16 +3,13 @@ module Views.CreateBallotV exposing (..)
 import Components.Btn exposing (BtnProps(..), btn)
 import Components.Icons exposing (IconSize(I24), mkIcon)
 import Components.TextF exposing (textF)
-import Dict
 import Element exposing (..)
 import Element.Attributes exposing (..)
 import Helpers exposing (dubCol, genNewId, getDemocracy, getField, getIntField, para)
-import Maybe.Extra exposing ((?))
 import Models exposing (Model)
 import Models.Ballot exposing (Ballot, BallotFieldIds, BallotId, BallotOption, BallotOptionFieldIds)
 import Models.Democracy exposing (Democracy, DemocracyId)
 import Msgs exposing (Msg(AddBallotToDemocracy, CreateBallot, MultiMsg, NavigateBack, NavigateBackTo, SetField, SetIntField))
-import Result as Result
 import Routes exposing (Route(DemocracyR))
 import Styles.Styles exposing (SvClass(NilS, SubH, SubSubH, VoteList))
 import Styles.Swarm exposing (scaled)
@@ -21,94 +18,53 @@ import Views.ViewHelpers exposing (SvElement, SvHeader, SvView)
 
 createBallotV : DemocracyId -> Model -> SvView
 createBallotV democId model =
-    let
-        democracy =
-            getDemocracy democId model
-    in
     ( empty
     , header
     , body democId model
     )
 
 
+header : SvHeader
+header =
+    ( []
+    , [ text "Create a ballot" ]
+    , []
+    )
+
+
 
 -- TODO: Add validation to all text fields etc.
 -- TODO: Check that vote is not in the past
---ballotFieldIds : BallotId -> BallotFieldIds
---ballotFieldIds ballotId =
---    let
---        idG x =
---            genNewId ballotId x
---    in
---    { name = idG 1
---    , desc = idG 2
---    , start = idG 3
---    , finish = idG 4
---    , numOpts = idG 5
---    }
---
---
---ballotOptionFieldIds : BallotId -> Int -> BallotOptionFieldIds
---ballotOptionFieldIds ballotId num =
---    let
---        idG x =
---            genNewId ballotId <| 6 + num * 3 + x
---    in
---    { id = idG 0
---    , name = idG 1
---    , desc = idG 2
---    }
 
 
-nameTextFId =
-    "ballot-name-tf"
+ballotFieldIds : BallotFieldIds
+ballotFieldIds =
+    { name = "ballot-name-tf"
+    , desc = "ballot-description-tf"
+    , startDate = "ballot-start-date-tf"
+    , startTime = "ballot-start-date-tf"
+    , durVal = "ballot-duration-value-tf"
+    , durType = "ballot-duration-type-tf"
+    , numOpts = "num-ballot-options-id"
+    }
 
 
-startDateTextFId =
-    "ballot-start-date-tf"
-
-
-startTimeTextFId =
-    "ballot-start-time-tf"
-
-
-durationValueTextFId =
-    "ballot-duration-value-tf"
-
-
-durationTypeTextFId =
-    "ballot-duration-type-tf"
-
-
-descriptionTextFId =
-    "ballot-description-tf"
-
-
-numBallotOptionsId =
-    "num-ballot-options-id"
-
-
-optionNameTextFId =
-    "ballot-option-name-tf"
-
-
-optionDescTextFId =
-    "ballot-option-description-tf"
+ballotOptionFieldIds : Int -> BallotOptionFieldIds
+ballotOptionFieldIds num =
+    { name = "ballot-option-name-tf-" ++ toString num
+    , desc = "ballot-option-description-tf-" ++ toString num
+    }
 
 
 body : DemocracyId -> Model -> SvElement
-body democracyId model =
-    let
-        democracy =
-            getDemocracy democracyId model
-    in
+body democId model =
     column NilS
         [ spacing (scaled 4) ]
         [ dubCol
             [ el SubH [] (text "Ballot Name")
             , para [] "Give your ballot a name, this will be the title that voters will see in the ballot list and also take prominent position on the voting screen."
             ]
-            [ textF nameTextFId "Ballot Name" model
+            [ textF ballotFieldIds.name "Ballot Name" model
             ]
         , dubCol
             [ el SubH [] (text "Start Date")
@@ -117,9 +73,9 @@ body democracyId model =
             [ row NilS
                 [ spacing (scaled 2) ]
                 [ mkIcon "calendar-range" I24
-                , textF durationValueTextFId "Select Date" model
+                , textF ballotFieldIds.startDate "Select Date" model
                 , mkIcon "clock" I24
-                , textF durationTypeTextFId "Start Time" model
+                , textF ballotFieldIds.startTime "Start Time" model
                 ]
             ]
         , dubCol
@@ -128,8 +84,8 @@ body democracyId model =
             ]
             [ row NilS
                 [ spacing (scaled 2) ]
-                [ textF startDateTextFId "1" model
-                , textF startTimeTextFId "Week(s)" model
+                [ textF ballotFieldIds.durVal "1" model
+                , textF ballotFieldIds.durType "Week(s)" model
                 ]
             , el NilS [ width fill ] (text " ")
             ]
@@ -137,82 +93,110 @@ body democracyId model =
             [ el SubH [] (text "Description")
             , para [] "Provide a description of the ballot, the purpose of the vote, what it will impact and how the decision will be made. Etc..."
             ]
-            [ textF descriptionTextFId "Description" model
+            [ textF ballotFieldIds.desc "Description" model
             ]
-        , el SubH [] (text "Ballot Options")
-        , column NilS [] (allBallotOptions model)
+        , ballotOptions model
+        , createNewBallot democId model
         ]
 
 
-allBallotOptions model =
+ballotOptions : Model -> SvElement
+ballotOptions model =
     let
         numBallotOptions =
-            List.range 0 <| getIntField numBallotOptionsId model + 1
+            List.range 0 <| getIntField ballotFieldIds.numOpts model + 1
+
+        addBallotOption =
+            SetIntField ballotFieldIds.numOpts <| getIntField ballotFieldIds.numOpts model + 1
+
+        removeBallotOption =
+            SetIntField ballotFieldIds.numOpts <| getIntField ballotFieldIds.numOpts model - 1
+
+        showRemoveOption =
+            if getIntField ballotFieldIds.numOpts model < 1 then
+                []
+            else
+                [ btn [ PriBtn, Small, Click removeBallotOption ] (text "- Remove an option") ]
+
+        allBallotOptions =
+            List.map ballotOptionView numBallotOptions
 
         ballotOptionView x =
-            let
-                indexStr =
-                    toString <| x + 1
-            in
-            el VoteList [ padding (scaled 2) ] <|
-                dubCol
-                    [ el NilS [ paddingBottom 20 ] (text <| "Option " ++ indexStr)
-                    , el SubSubH [] (text "Name")
-                    , textF (optionNameTextFId ++ indexStr) "Option Name" model
+            column VoteList
+                [ paddingBottom (scaled 2) ]
+                [ el NilS [ paddingXY 0 10 ] (text <| "Option " ++ (toString <| x + 1))
+                , dubCol
+                    -- [ el NilS [ paddingXY 0 10 ] (text <| "Option " ++ indexStr)
+                    [ el SubSubH [] (text "Name")
+                    , textF (ballotOptionFieldIds x).name "Option Name" model
                     ]
-                    [ btn [ Attr alignRight ] (text "x Remove Option")
-                    , el SubSubH [] (text "Description")
-                    , textF (optionDescTextFId ++ indexStr) "Description" model
+                    -- [ btn [ Attr alignRight ] (text "x Remove Option")
+                    [ el SubSubH [] (text "Description")
+                    , textF (ballotOptionFieldIds x).desc "Description" model
                     ]
+                ]
     in
-    List.map ballotOptionView numBallotOptions
+    column NilS
+        [ spacing (scaled 2) ]
+        [ el SubH [] (text "Ballot Options")
+        , column NilS [] allBallotOptions
+        , row NilS
+            [ spacing (scaled 2) ]
+          <|
+            [ btn [ PriBtn, Small, Click addBallotOption ] (text "+ Add another option")
+            ]
+                ++ showRemoveOption
+        ]
+
+
+createNewBallot : DemocracyId -> Model -> SvElement
+createNewBallot democId model =
+    let
+        democracy =
+            getDemocracy democId model
+
+        ballotId =
+            genNewId democId <| List.length democracy.ballots
+
+        numBallotOptions =
+            List.range 0 <| getIntField ballotFieldIds.numOpts model + 1
+
+        newBallotOption x =
+            { id = ballotId + x + 1
+            , name = getField (ballotOptionFieldIds x).name model
+            , desc = getField (ballotOptionFieldIds x).desc model
+            , result = Nothing
+            }
+
+        newBallot =
+            { name = getField ballotFieldIds.name model
+            , desc = getField ballotFieldIds.desc model
+
+            --            TODO: Implement date fields.
+            --            , start = Result.withDefault 0 <| String.toFloat <| getField ballotField.start model
+            --            , finish = Result.withDefault 0 <| String.toFloat <| getField ballotField.finish model
+            , start = 0
+            , finish = 0
+            , ballotOptions = List.map newBallotOption numBallotOptions
+            }
+
+        completeMsg =
+            MultiMsg
+                [ CreateBallot newBallot ballotId
+                , AddBallotToDemocracy ballotId democId
+                , NavigateBackTo <| DemocracyR democId
+                ]
+    in
+    --    TODO: Replace placeholder text
+    dubCol
+        [ el SubH [] (text "Complete")
+        , para [] "Your ballot, <ballot name>, will commence on the <date> at <time>. The ballot will run for <duration> and is made up of <number of options> options."
+        , btn [ PriBtn, Small, Click completeMsg ] (text "Create new ballot")
+        ]
+        []
 
 
 
---        ballotId =
---            genNewId democracyId <| List.length democracy.ballots
---
---        ballotField =
---            ballotFieldIds ballotId
---
---        ballotOptionField num =
---            ballotOptionFieldIds ballotId num
---
---        ballotOptionView x =
---            div [ class "ba pa3 ma3" ]
---                [ styled p [ Typo.subhead ] [ text <| "Option " ++ (toString <| x + 1) ]
---                , textF (ballotOptionField x).name "Name" [] model
---                , textF (ballotOptionField x).desc "Description" [ Textf.textarea ] model
---                ]
---
---        numBallotOptions =
---            List.range 0 <| getIntField ballotField.numOpts model + 1
---
---        allBallotOptions =
---            map ballotOptionView numBallotOptions
---
---        newBallotOption x =
---            { id = (ballotOptionField x).id
---            , name = getField (ballotOptionField x).name model
---            , desc = getField (ballotOptionField x).desc model
---            , result = Nothing
---            }
---
---        newBallot =
---            { name = getField ballotField.name model
---            , desc = getField ballotField.desc model
---            , start = Result.withDefault 0 <| String.toFloat <| getField ballotField.start model
---            , finish = Result.withDefault 0 <| String.toFloat <| getField ballotField.finish model
---            , ballotOptions = map newBallotOption numBallotOptions
---            }
---
---        completeMsg =
---            MultiMsg
---                [ CreateBallot newBallot ballotId
---                , AddBallotToDemocracy ballotId democracyId
---                , NavigateBackTo <| DemocracyR democracyId
---                , ShowToast <| newBallot.name ++ " has been created."
---                ]
 --
 --        errorTimeFormat timeId =
 --            Textf.error "Please enter an Epoch time"
@@ -225,39 +209,3 @@ allBallotOptions model =
 --                            False
 --                    )
 --
---        removeDisabled =
---            if getIntField ballotField.numOpts model < 1 then
---                [ Disabled ]
---            else
---                []
---
---        addBallotOption =
---            SetIntField ballotField.numOpts <| getIntField ballotField.numOpts model + 1
---
---        removeBallotOption =
---            SetIntField ballotField.numOpts <| getIntField ballotField.numOpts model - 1
---    in
---    div [ class "pa4" ] <|
---        [ styled p [ Typo.subhead ] [ text "Ballot Details:" ]
---        , textF ballotField.name "Name" [] model
---        , textF ballotField.desc "Description" [ Textf.textarea ] model
---        , textF ballotField.start "Start Time" [ errorTimeFormat ballotField.start ] model
---        , textF ballotField.finish "Finish Time" [ errorTimeFormat ballotField.finish ] model
---        , styled p [ cs "mt4", Typo.subhead ] [ text "Ballot Options:" ]
---        ]
---            ++ allBallotOptions
---            ++ [ btn 574567456755 model [ Icon, Attr (class "sv-button-large dib"), Click addBallotOption ] [ Icon.view "add_circle_outline" [ Icon.size36 ] ]
---               , btn 347584445667 model ([ Icon, Attr (class "sv-button-large dib"), Click removeBallotOption ] ++ removeDisabled) [ Icon.view "remove_circle_outline" [ Icon.size36 ] ]
---               , div [ class "mt4" ]
---                    [ btn 97546756756 model [ SecBtn, Attr (class "ma3 dib"), Click NavigateBack ] [ text "Cancel" ]
---                    , btn 85687456456 model [ PriBtn, Attr (class "ma3 dib"), Click completeMsg ] [ text "Create" ]
---                    ]
---               ]
-
-
-header : SvHeader
-header =
-    ( []
-    , [ text <| "Create a ballot" ]
-    , []
-    )
