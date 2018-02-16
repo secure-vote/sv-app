43a6ef0ffd7dba16a23fc29a433b91750b49dc00
Thomas Sesselmann <sessel52@gmail.com>
2018-02-16 14:47:46 +1100

    Refactor JS files.

--- web/index-demo.js
+++ web/index-demo.js
@@ -1,5 +1,32 @@
 'use strict';
 
-var Elm = require('./src/MainDemo.elm');
+const loadElm = function() {
 
-window['Elm'] = Elm;
+    const Elm = require('./src/MainDemo.elm');
+
+    if (typeof Elm !== "undefined") {
+
+        window['Elm'] = Elm;
+
+        var node = document.getElementById('sv-fullscreen');
+        var flags = {
+            votingPrivKey: '', //Uint8Array.from(Array(32).fill(0)),
+            democracyId: 31,
+            admin: true,
+            singleDemocName: "SV Demo"
+        };
+
+        const app = Elm.MainDemo.embed(node, flags);
+
+        initialisePorts(app);
+
+        // Remove Loading Spinner
+        var spinner = document.getElementById("loading-screen");
+        spinner.parentNode.removeChild(spinner);
+
+    } else {
+        setTimeout(loadElm, 100);
+    }
+}
+
+loadElm();
--- web/src/Ports.js
+++ web/src/Ports.js
@@ -1,9 +1,79 @@
-
-// This file is not being used yet
-
 const initialisePorts = (app) => {
 
-    app.ports.writeLocalStorageImpl(({key, value}) => {
-        localStorage.setItem(key, JSON.stringify(value))
-    })
+    // app.ports.writeLocalStorageImpl(({key, value}) => {
+    //     localStorage.setItem(key, JSON.stringify(value))
+    // })
+
+    app.ports.sendBcData.subscribe(errWrapper("sendBcData")(sendDataToBlockchain));
+    app.ports.getBcData.subscribe(errWrapper("getBcData")(getDataFromBlockchain));
+    app.ports.writeLsImpl.subscribe(errWrapper("writeLsImpl")(writeToLocalStorage));
+    app.ports.readLsImpl.subscribe(errWrapper("readLsImpl")(readFromLocalStorage));
+
+
+
+    function sendDataToBlockchain(arg) {
+
+        var [ref, data] = arg;
+
+        // Talk to Blockchain
+        setTimeout(function(){
+
+            // Generate Receipt
+            var txId = generateReceipt(data);
+
+            // Send Receipt
+            app.ports.receiptBcData.send([ref, txId]);
+
+            // Wait for Blockchain Confirmation
+            setTimeout(function(){
+
+                // Send Blockchain Confirmation
+                app.ports.confirmBcData.send(ref);
+            }, 5000);
+
+        }, 3000);
+
+    }
+
+    function getDataFromBlockchain(txId) {
+
+        // Talk to Blockchain
+        setTimeout(function(){
+
+            // Get Data
+            var data = "DATAAAAAA";
+
+            // Send Receipt
+            app.ports.receiveBcData.send(data);
+
+        }, 3000);
+
+    }
+
+    function generateReceipt(data) {
+        var receipt = Math.floor(Math.random() * 1000000).toString()
+        return receipt;
+    }
+
+    function writeToLocalStorage({key, value}) {
+        localStorage.setItem("sv_" + key, JSON.stringify(value))
+    }
+
+    function readFromLocalStorage(key) {
+        var ls = localStorage.getItem("sv_" + key)
+        app.ports.gotLsImpl.send({key: key, value: JSON.parse(ls)});
+    }
+
+    function errWrapper(fName) {
+        return function(f) {
+            return function(...args){
+                try {
+                    return f.apply(this, args);
+                } catch (err) {
+                    // error handling here
+                    console.error("Function", fName, "produced error:", err);
+                }
+            }
+        }
+    }
 }
